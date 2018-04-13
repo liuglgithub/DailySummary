4e3e1e596584bfd872188c458088745171fba7fd
liugl <liuglxa@163.com>
2018-04-13 10:40:05 +0800

    添加注释

--- app/src/main/java/com/liugl/alltest/view/sharepbg/SharepActivity.java
+++ app/src/main/java/com/liugl/alltest/view/sharepbg/SharepActivity.java
@@ -1,6 +1,7 @@
 package com.liugl.alltest.view.sharepbg;
 
 import android.graphics.Outline;
+import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,14 +25,27 @@ private ImageView  outlineProImg;
 
 
 
+//        outlineProImg.setElevation(5);
 
         ViewOutlineProvider viewOutlineProvider  = new ViewOutlineProvider() {
             @Override
             public void getOutline(View view, Outline outline) {
                 //x，y轴表示位置，后两个参数表示长，宽
                 outline.setOval(0,0,outlineProImg.getWidth(),outlineProImg.getHeight());
+                //你可以用 Path 指定任何的形状，前提是凸多边形
+                //这里设置投影的位置从右下角开始，投影形状是矩形
+//                Path path = new Path();
+//                path.moveTo(view.getWidth(), view.getHeight());
+//                path.lineTo(view.getWidth(), view.getHeight() * 2);
+//                path.lineTo(view.getWidth() * 2, view.getHeight() * 2);
+//                path.lineTo(view.getWidth() * 2, view.getHeight());
+//                path.close();
+//                outline.setConvexPath(path);
             }
         };
-        outlineProImg.setOutlineProvider(viewOutlineProvider);
+//        outlineProImg.setOutlineProvider(viewOutlineProvider);
+//        outlineProImg.setOutlineProvider(viewOutlineProvider);
+//        outlineProImg.setClipToOutline( true);
+
     }
 }
