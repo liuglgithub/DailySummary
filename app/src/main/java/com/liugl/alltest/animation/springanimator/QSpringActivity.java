e4ffe24b92913d4d4f54408594b7ec4cb77fc947
liugl <liuglxa@163.com>
2018-05-31 17:41:03 +0800

    弹性动画03

---
+++ app/src/main/java/com/liugl/alltest/animation/springanimator/QSpringActivity.java
@@ -0,0 +1,111 @@
+package com.liugl.alltest.animation.springanimator;
+
+import android.support.animation.DynamicAnimation;
+import android.support.animation.SpringAnimation;
+import android.support.animation.SpringForce;
+import android.support.annotation.FloatRange;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+
+import com.liugl.alltest.R;
+
+public class QSpringActivity extends AppCompatActivity {
+
+    ImageView movingView;
+
+
+    float STIFFNESS = SpringForce.STIFFNESS_MEDIUM;//硬度
+    float DAMPING_RATIO = SpringForce.DAMPING_RATIO_HIGH_BOUNCY;//阻尼
+
+    SpringAnimation xAnimation;//x方向
+    SpringAnimation yAnimation;//y方向
+
+
+    float dX = 0f;
+    float dY = 0f;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qspring);
+        movingView = findViewById(R.id.animation_image);
+
+
+        // 以图片的初始位置创建动画对象
+        movingView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                xAnimation = createSpringAnimation(
+                        movingView, SpringAnimation.X, movingView.getX(), STIFFNESS, DAMPING_RATIO);
+                yAnimation = createSpringAnimation(
+                        movingView, SpringAnimation.Y, movingView.getY(), STIFFNESS, DAMPING_RATIO);
+                //初始位置确定，移除监听
+                movingView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
+        movingView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // 计算到左上角的距离
+                        dX = view.getX() - event.getRawX();
+                        dY = view.getY() - event.getRawY();
+
+                        // 取消动画以便按住图片
+                        xAnimation.cancel();
+                        yAnimation.cancel();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        //  另一种改变View的LayoutParams（位置）的方式
+                        movingView.animate()
+                                .x(event.getRawX() + dX)
+                                .y(event.getRawY() + dY)
+                                .setDuration(0)
+
+                                .start();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        xAnimation.start();
+                        yAnimation.start();
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+
+
+    /**
+     * 创建弹性动画
+     *
+     * @param view          动画关联的控件
+     * @param property      动画作用的属性
+     * @param finalPosition 动画结束的位置
+     * @param stiffness     硬度
+     * @param dampingRatio  阻尼
+     * @return
+     */
+    SpringAnimation createSpringAnimation(View view,
+                                          DynamicAnimation.ViewProperty property,
+                                          Float finalPosition,
+                                          @FloatRange(from = 0.1) Float stiffness,
+                                          @FloatRange(from = 0.0) Float dampingRatio) {
+        //创建弹性动画类SpringAnimation
+        SpringAnimation animation = new SpringAnimation(view, property);
+        //SpringForce类，定义弹性特质
+        SpringForce spring = new SpringForce(finalPosition);
+        spring.setStiffness(stiffness);
+        spring.setDampingRatio(dampingRatio);
+        //关联弹性特质
+        animation.setSpring(spring);
+        return animation;
+    }
+}
--- app/src/main/java/com/liugl/alltest/animation/springanimator/SpringAllActivity.java
+++ app/src/main/java/com/liugl/alltest/animation/springanimator/SpringAllActivity.java
@@ -12,24 +12,28 @@ public class SpringAllActivity extends AppCompatActivity implements View.OnClick
 
 
     Button spring_01;
+    Button spring_02;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spring_all);
         spring_01 = (Button) this.findViewById(R.id.spring_01);
+        spring_02 = (Button) this.findViewById(R.id.spring_02);
 
         spring_01.setOnClickListener(this);
+        spring_02.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.spring_01:
-
                 Intent springIntent = new Intent(this,SpringOneActivity.class);
                 this.startActivity(springIntent);
-
-
+                break;
+            case R.id.spring_02:
+                Intent qspringIntent = new Intent(this,QSpringActivity.class);
+                this.startActivity(qspringIntent);
                 break;
         }
     }
